a02c8e8fd1791b3516e7b5dd3ee10509503134b0
Dj <deshawnjohal@gmail.com>
2026-09-11 00:20:35 +0100

    Implement stepper motor control functions and initialize GPIO pins

--- include/components/stepper.h
+++ include/components/stepper.h
@@ -4,6 +4,13 @@
 
 
 
+#define STEPPER_PIN1 GPIO_NUM_12
+#define STEPPER_PIN2 GPIO_NUM_14
+#define STEPPER_PIN3 GPIO_NUM_27
+#define STEPPER_PIN4 GPIO_NUM_26
+
+void Stepper_Init();
+void Stepper_Movement();
 
 
 
